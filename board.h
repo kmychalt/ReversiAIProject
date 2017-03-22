9dba4529b3c5d3740a7ce24c92a254d03f4f09b2
Kristin Thomas <kmychalt@gmail.com>
2017-03-21 22:47:18 -0700

    Added getFitness() and commentary

--- board.h
+++ board.h
@@ -3,14 +3,17 @@
 #define	BOARD_H
 
 #define BOARD_SIZE 8
+#define NUM_OF_COLORS 2
 
 class Board {
 public:
     Board();
+    int& getFitness();
     int getFitness(int color);
     void printBoard();
 private:
     int board[BOARD_SIZE][BOARD_SIZE];
+    int fitness[NUM_OF_COLORS];
 };
 
 #endif	
